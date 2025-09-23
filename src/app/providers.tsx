304e129dfa40049b082951931913644b79fb6dd0
Brayen Luhat <bfenomenal@gmail.com>
2025-09-23 21:44:42 +0800

    add: new push

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,8 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+'use client'
+
+import { SessionProvider } from 'next-auth/react'
+import { ReactNode } from 'react'
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider>{children}</SessionProvider>
 }
